03e86847a678060315f2be37a043c1a424f7f2b1
Shun's Vineyard <shunsvineyard@protonmail.com>
2020-12-13 14:56:31 -0800

    Added installation dependencies.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setuptools.setup(
     ],
     keywords="IoT",
     packages=setuptools.find_packages(exclude=["examples", "tests"]),
+    install_requires=["click", "opencv-python", "psutil"],
     entry_points={"console_scripts": ["iot-health-cli=iothealth.bin.cli:main"]},
     python_requires=">=3.7",
 )
